f3916c97904fd452d6e74f09df888dffd47b28d2
GIRARD Antoine <antoine.girard@datag.fr>
2025-09-16 11:14:26 +0200

    Mise à jour de l'application : interface améliorée et sorties en tableau et graphique

--- server.R
+++ server.R
@@ -2,23 +2,35 @@
 library(shiny)
 
 
+
 function(input, output, session) {
 
+    # Fonction réactive "intermédiaire" pour la sélection du périmètre choisi par l'utilisateur
+     # Eventreactive : déclenchement seulement au clic sur boutton
+  perimetre <- eventReactive(input$go, {
+    
+    if (input$choix_genre != "Tous les genres") {
+      data_allocine_plot <- data_allocine %>% 
+        filter(genre == input$choix_genre)  # Filtrer sur le genre choisi par l'utilisateur
+    }
+    
+    else { 
+      data_allocine_plot <- data_allocine  # Pas de filtre si "tous les genres" choisis
+    }
+    
+    if (input$choix_reprise == FALSE) {           # Exclure les films repris si la boite est cochée
+      data_allocine_plot <- data_allocine_plot %>% 
+        filter(reprise != TRUE)
+    }
+    
+    return(data_allocine_plot)
+    
+  })
+  
     output$plot_evolution <- renderPlot({
 
-      
-        # Graphique sur l'évolution du nombre de films par an (sur un genre donné)
-      
-      if (input$choix_genre != "Tous les genres") {
-        data_allocine_plot <- data_allocine %>% 
-          filter(genre == input$choix_genre)  # Filtrer sur le genre choisi par l'utilisateur
-      }
-      
-      else { 
-          data_allocine_plot <- data_allocine  # Pas de filtre si "tous les genres" choisis
-      }
-
-      data_allocine_plot %>% 
+     
+      perimetre() %>% # On repart de la fonction réactive de périmètre
         mutate(annee_sortie = year(date_sortie)) %>% 
         count(annee_sortie) %>% 
         ggplot() + 
@@ -28,4 +40,17 @@ function(input, output, session) {
       
     })
 
+    output$table_evolution <- renderTable({
+      
+
+      perimetre() %>%  # On repart de la fonction réactive de périmètre
+        mutate(annee_sortie = year(date_sortie)) %>% 
+        count(annee_sortie)
+      
+    })
+    
+
 }
+
+
+
--- ui.R
+++ ui.R
@@ -1,26 +1,48 @@
 
 
 library(shiny)
+library(shinythemes)
 
 # Define UI for application that draws a histogram
 fluidPage(
-
-    # Application title
-    titlePanel("Application Allociné"),
-
-    # Barre latérale avec un bouton de choix du genre
-    sidebarLayout(
-        sidebarPanel(
-           
-          selectInput("choix_genre", "Choix du genre : ", 
-          choices = c("Tous les genres", unique(data_allocine$genre)))
-          
-          
-        ),
-
-        # Afficher le graphique d'évolution du nombre de films par an
-        mainPanel(
-            plotOutput("plot_evolution")
-        )
-    )
+  theme = shinytheme("lumen"),
+   #themeSelector(), # CHoix du theme (pour tester un thème)
+  # Pour un thème plus personnalisé, utiliser le package bslib
+  
+  navbarPage("Application Allociné",  # Structure avec barre de navigation
+             
+             # Logo allociné
+             img(src = "logo_allocine.jpeg", height = "100x"),
+             
+             tabPanel("Page principale",  # Page principal
+                      
+               
+                      # Barre latérale avec un bouton de choix du genre
+                      sidebarLayout(
+                        sidebarPanel(
+                          
+                          selectInput("choix_genre", "Choix du genre : ", 
+                                      choices = c("Tous les genres", unique(data_allocine$genre))
+                          ),
+                          checkboxInput("choix_reprise", "Inclure les films repris"),
+                          actionButton("go", "Valider")
+                          
+                          
+                        ),
+                        
+                        # Afficher le graphique d'évolution du nombre de films par an
+                        mainPanel(
+                          tabsetPanel(
+                            tabPanel("Graphique", plotOutput("plot_evolution")),
+                            tabPanel("Tableau", tableOutput("table_evolution"))
+                          )
+                        )
+                      )
+             ),
+             tabPanel("A propos",  # Page secondaire
+                      "Ceci est une application d'exemple basée sur les données", strong("Allociné")
+             )
+             
+  )
 )
+
